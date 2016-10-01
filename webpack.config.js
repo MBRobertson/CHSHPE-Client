ff217ee6173ffc7bc37e107999c6cebf53facd02
TheImpersonator <mbr.robertson@gmail.com>
2016-10-01 18:55:17 +1300

    More webpack config
    This'll be the one. I can feel it.

--- webpack.config.js
+++ webpack.config.js
@@ -5,15 +5,23 @@ var BUILD_DIR = path.resolve(__dirname, 'www');
 var APP_DIR = path.resolve(__dirname, 'src');
 
 var TRAVIS = process.env.TRAVIS ? JSON.parse(process.env.TRAVIS) : false;
+var plugins = [
+    new webpack.optimize.UglifyJsPlugin({
+        compress: {
+            warnings: false
+        }
+    })
+];
 
 if (TRAVIS) {
-  console.log('TRAVIS mode (will fail on error)');
-  plugins.push(new webpack.NoErrorsPlugin());
+    console.log('TRAVIS mode (will fail on error)');
+    plugins.push(new webpack.NoErrorsPlugin());
 }
 
 var config = {
     bail: TRAVIS,
     entry: path.join(APP_DIR, 'app.jsx'),
+    plugins: plugins,
     output: {
         path: BUILD_DIR,
         filename: 'js/app.js'
