9d1bb0ab4b2ef57327f535fdc9d28d7701f318f1
TheImpersonator <mbr.robertson@gmail.com>
2016-12-02 12:03:39 +1300

    Better prettier transitions (again v2)

--- src/app.jsx
+++ src/app.jsx
@@ -21,7 +21,7 @@ class App extends React.Component {
         return (
             <main>
                 <Nav loc={this.props.location}/>
-                <ReactCSSTransitionGroup transitionName="pageSwap" transitionEnterTimeout={250} transitionLeaveTimeout={250}>
+                <ReactCSSTransitionGroup transitionName="pageSwap" transitionEnterTimeout={225} transitionLeaveTimeout={225}>
                     <section id="mainContent" key={this.props.location.pathname}>
                         {this.props.children}
                     </section>
