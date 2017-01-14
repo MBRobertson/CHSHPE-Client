afbdfc6d6d426909a7d61421d72b662229ab05f8
Michael Robertson <mbr.robertson@gmail.com>
2017-01-15 11:36:20 +1300

    Update server location

--- src/config.jsx
+++ src/config.jsx
@@ -2,7 +2,8 @@ import 'whatwg-fetch';
 
 var config = {
     //api: 'http://localhost:3001/api'
-    api: 'http://chspeserver-mbrobertson.rhcloud.com/api'
+    //api: 'http://chspeserver-mbrobertson.rhcloud.com/api'
+    api: 'http://13.55.87.144/api'
 }
 
 let teachers = {
